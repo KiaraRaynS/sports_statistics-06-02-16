40c1bc0acad8d6902b262cf6f22cb9206029773e
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-02 17:29:08 -0400

    linux computer lives

--- sports_statistics_work.py
+++ sports_statistics_work.py
@@ -19,10 +19,20 @@ cursor.execute(generate_table)
 cursor.execute("INSERT INTO players_table VALUES ('Arteezy', 'Team Secret', 1, 8720, 'Shadow Fiend', '56.07%');")
 cursor.execute("INSERT INTO players_table VALUES ('EternaLEnVy', 'Team Secret', 2, 7841, 'Storm Spirit', '56.41%');")
 cursor.execute("INSERT INTO players_table VALUES ('UNiVeRsE', 'Team Secret', 3, 7775, 'Dark Seer', '69.70%');")
-cursor.execute("INSERT INTO players_table VALUES ('Puppey', 'Team Secret', 4, 7000, 'Chen', '70.80%');")
-cursor.execute("INSERT INTO players_table VALUES ('pieliedie', 'Team Secret', 5, 7757, 'Bounty Hunter', '64.52');")
+cursor.execute("INSERT INTO players_table VALUES ('Puppey', 'Team Secret', 4, 6935, 'Chen', '70.80%');")
+cursor.execute("INSERT INTO players_table VALUES ('pieliedie', 'Team Secret', 5, 7757, 'Bounty Hunter', '64.52%');")
 
 # Natus Vincere
+cursor.execute("INSERT INTO players_table VALUES ('Ditya Ra', 'Natus Vincere', 1, 7245, 'Ember Spirit', '57.86%');")
+cursor.execute("INSERT INTO players_table VALUES ('Dendi', 'Natus Vincere' 2, 7128, 'Pudge', '56.59%');")
+cursor.execute("INSERT INTO players_table VALUES ('GeneRal', 'Natus Vincere', 3, 6930, 'Shadow Fiend', '53.83%');")
+cursor.execute("INSERT INTO players_table VALUES ('SoNNeikO', 'Natus Vincere', 4, 8075, 'Io', '53.99%');")
+cursor.execute("INSERT INTO players_table VALUES ('Artstyle', 'Natus Vincere', 5, 0000, 'Chen', '58.82%');")
+
+# Alliance
+cursor.execute("INSERT INTO players_table VALUES ('Loda', 'Alliance', 1, 0000, 'Juggernaut', '66.19%');")
+
+# Newbee
 
 # Evil Geniuses
 cursor.execute("INSERT INTO players_table VALUES ('Aui_2000', 'Evil Geniuses', 1, 7628, 'Morphling', '72.70%');")
